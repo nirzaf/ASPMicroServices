af152bb9ea830ee3f3f993d06944025544ae87bf
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-22 11:12:22 +0530

    EF core configured

---
+++ AccountService/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountService.Models
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}
---
+++ AccountService/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountService.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
+}
